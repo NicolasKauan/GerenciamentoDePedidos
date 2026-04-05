c343cf802462835d834f1ab256d13da773c5143b
Nicolas Kauan <166877999+NicolasKauan@users.noreply.github.com>
2026-04-05 17:08:35 -0300

    Criação do model

---
+++ src/main/java/com/nika/GerenciamentoDePedidos/model/PedidoModel.java
@@ -0,0 +1,55 @@
+package com.nika.GerenciamentoDePedidos.model;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity(name = "ProdutosModel")
+@Table(name = "tb_pedidos")
+public class PedidoModel {
+    @id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "DataHoraPedido")
+    private LocalDateTime data;
+
+    @Column(name = "ValorTotalPedido")
+    private BigDecimal valorTotal;
+
+    @Column(name = "StatusPedido")
+    private String status;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getData() {
+        return data;
+    }
+
+    public void setData(LocalDateTime data) {
+        this.data = data;
+    }
+
+    public BigDecimal getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(BigDecimal valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
---
+++ src/test/java/com/nika/GerenciamentoDePedidos/GerenciamentoDePedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nika.GerenciamentoDePedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoDePedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
